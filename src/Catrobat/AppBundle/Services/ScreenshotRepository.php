1573d4846d6d2bd8dc0902ad008203ef2e95db3b
Andrej Knaus <andrej.knaus@student.tugraz.at>
2017-05-19 18:37:00 +0200

    commit #3

--- src/Catrobat/AppBundle/Services/ScreenshotRepository.php
+++ src/Catrobat/AppBundle/Services/ScreenshotRepository.php
@@ -156,6 +156,8 @@ class ScreenshotRepository
   {
     $filesystem = new Filesystem();
     $screenshot_temp_path = $this->tmp_dir . $this->generateFileNameFromId($id);
+    $filesystem->chown($screenshot_temp_path, "unpriv");
+    $filesystem->chgrp($screenshot_temp_path, "unpriv");
     $filesystem->chmod($screenshot_temp_path, 0766);
     $filesystem->copy($screenshot_temp_path, $this->screenshot_dir . $this->generateFileNameFromId($id));
     $filesystem->remove($screenshot_temp_path);
@@ -165,6 +167,8 @@ class ScreenshotRepository
   {
     $filesystem = new Filesystem();
     $thumbnail_temp_path = $this->tmp_dir . "thumb/" . $this->generateFileNameFromId($id);
+    $filesystem->chown($thumbnail_temp_path, "unpriv");
+    $filesystem->chgrp($thumbnail_temp_path, "unpriv");
     $filesystem->chmod($thumbnail_temp_path, 0766);
     $filesystem->copy($thumbnail_temp_path, $this->thumbnail_dir . $this->generateFileNameFromId($id));
     $filesystem->remove($thumbnail_temp_path);
